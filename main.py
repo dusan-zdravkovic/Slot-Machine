7608a29adcfbd111ebb7d5ea27dfea9f684a7128
Dusan Zdravkovic <dusanz03@gmail.com>
2025-07-19 23:48:37 -0400

    applied suggestion

--- main.py
+++ main.py
@@ -11,3 +11,19 @@ def deposit():
             print("Please enter a number.")
 
     return amount
+
+
+MAX_LINES = 3
+
+
+def get_number_of_lines():
+    while True:
+        lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}): ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                return lines
+            else:
+                print(f"Enter a number between 1 and {MAX_LINES}.")
+        else:
+            print("Please enter a valid number.")
